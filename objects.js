3217fb61a2c9ed2a3fe7399f0f712b84ca329b38
Benjamin Goza <bengoza@gmail.com>
2019-07-16 14:43:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,8 @@
 var playlist = {"Aphex Twin": "Flim"};
 //Object.assign({playlist}, {"Aphex Twin": "Flim"})
+
+function updatePlaylist(obj, key, value) {
+  playlist["Lost Tribe"] = Gamemaster;
+  
+  return playlist;
+}
